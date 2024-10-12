814d9018048511160e89214d56459b2717607433
Shivakant tripathi <shivatripathi559@gmail.com>
2024-10-12 14:10:49 +0530

    Initial commit

---
+++ mern_development/Front-end-tech/app.js
@@ -0,0 +1,55 @@
+// console.log('hello world');
+// document.getElementById('myh1').textContent='Hello World';
+// document.getElementById('myP').textContent="hello my name is god";
+
+//1 Basic Understanding of Variable  *************
+
+// let a=10;
+// let b=20;
+// let c=a+b;
+// console.log("Sum of these numbers =" + c);\
+
+
+
+
+// const IP=3.14;
+// let redius;
+// let circumference;
+
+// document.getElementById('mysubmit').onclick=function(){
+//     redius=document.getElementById('radius').value;
+//     circumference=2*IP*redius;
+//     document.getElementById
+    
+// }
+
+// function myfuntion(){
+
+//     document.getElementById('Demo').innerText=Date();
+
+// }
+
+// How to turn bulb 
+
+// function myfuntion(){
+
+//     document.getElementById('myimg').innerHTML=pic_bulboff.gif;
+
+// }
+
+// function myfuntion() {
+//     document.getElementById('img').style=display='block';
+// }
+
+// function myfuntion1(){
+//     document.getElementById('img').style=display='inline-block';
+// }
+
+let a=10;
+let b=3;
+let c=234;
+console.log(a?b:c);
+
+
+// myfuntion1()
+
